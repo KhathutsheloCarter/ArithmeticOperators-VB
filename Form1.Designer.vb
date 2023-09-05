e8d3eaa17e42bf74917b666fd2165de4a9d129a6
KhathutsheloCarter <121690232+KhathutsheloCarter@users.noreply.github.com>
2023-09-05 15:03:29 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFirstNumber = New System.Windows.Forms.Label()
+        Me.txtFirstNumber = New System.Windows.Forms.TextBox()
+        Me.lblSecondNumber = New System.Windows.Forms.Label()
+        Me.txtSecondNumber = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblFirstNumber
+        '
+        Me.lblFirstNumber.AutoSize = True
+        Me.lblFirstNumber.Location = New System.Drawing.Point(284, 72)
+        Me.lblFirstNumber.Name = "lblFirstNumber"
+        Me.lblFirstNumber.Size = New System.Drawing.Size(157, 25)
+        Me.lblFirstNumber.TabIndex = 0
+        Me.lblFirstNumber.Text = "Enter first Number"
+        '
+        'txtFirstNumber
+        '
+        Me.txtFirstNumber.Location = New System.Drawing.Point(459, 66)
+        Me.txtFirstNumber.Name = "txtFirstNumber"
+        Me.txtFirstNumber.Size = New System.Drawing.Size(150, 31)
+        Me.txtFirstNumber.TabIndex = 1
+        '
+        'lblSecondNumber
+        '
+        Me.lblSecondNumber.AutoSize = True
+        Me.lblSecondNumber.Location = New System.Drawing.Point(249, 150)
+        Me.lblSecondNumber.Name = "lblSecondNumber"
+        Me.lblSecondNumber.Size = New System.Drawing.Size(181, 25)
+        Me.lblSecondNumber.TabIndex = 0
+        Me.lblSecondNumber.Text = "Enter second number"
+        '
+        'txtSecondNumber
+        '
+        Me.txtSecondNumber.Location = New System.Drawing.Point(459, 147)
+        Me.txtSecondNumber.Name = "txtSecondNumber"
+        Me.txtSecondNumber.Size = New System.Drawing.Size(150, 31)
+        Me.txtSecondNumber.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(328, 301)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(206, 86)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtSecondNumber)
+        Me.Controls.Add(Me.txtFirstNumber)
+        Me.Controls.Add(Me.lblSecondNumber)
+        Me.Controls.Add(Me.lblFirstNumber)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblFirstNumber As Label
+    Friend WithEvents txtFirstNumber As TextBox
+    Friend WithEvents lblSecondNumber As Label
+    Friend WithEvents txtSecondNumber As TextBox
+    Friend WithEvents btnCalculate As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNumber.TextChanged, txtSecondNumber.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblFirstNumber.Click, lblSecondNumber.Click
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+
+
+        Dim FirstNumber As Integer
+        Dim SecondNumber As Integer
+        Dim Answer As Integer
+
+        FirstNumber = txtFirstNumber.Text
+        SecondNumber = txtSecondNumber.Text
+
+        'Addition
+        Answer = FirstNumber + SecondNumber
+        MessageBox.Show(FirstNumber & "+" & SecondNumber & "=" & "" & Answer)
+
+        'Subtration
+        Answer = FirstNumber - SecondNumber
+        MessageBox.Show(FirstNumber & "-" & SecondNumber & "=" & "" & Answer)
+
+        'Multiplication
+        Answer = FirstNumber * SecondNumber
+        MessageBox.Show(FirstNumber & "*" & SecondNumber & "=" & "" & Answer)
+
+        'Division
+        Answer = FirstNumber / SecondNumber
+        MessageBox.Show(FirstNumber & "/" & SecondNumber & "=" & "" & Answer)
+
+        'Power
+        Answer = FirstNumber ^ SecondNumber
+        MessageBox.Show(FirstNumber & "^" & SecondNumber & "=" & "" & Answer)
+
+        'Integer Division
+        Answer = FirstNumber \ SecondNumber
+        MessageBox.Show(FirstNumber & "\" & SecondNumber & "=" & "" & Answer)
+
+
+
+    End Sub
+End Class
